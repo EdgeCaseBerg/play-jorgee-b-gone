5656fce339227e2adc278ca05e5885a2fdeae4e8
Ethan Eldridge <Ethan.Eldridge@meredith.com>
2017-08-15 12:17:38 -0400

    formatting

--- play24/app/module/JorgeeBGoneModule.scala
+++ play24/app/module/JorgeeBGoneModule.scala
@@ -15,27 +15,27 @@ import javax.inject.{ Inject, Named }
 import com.github.edgecaseberg.jorgeebgone._
 
 class JorgeeBGoneUserAgentFilter @Inject() (
-    blacklist: BlacklistProvider,
-    @Named("jorgeebgone.redirectTo") redirectTo: String
+		blacklist: BlacklistProvider,
+		@Named("jorgeebgone.redirectTo") redirectTo: String
 ) extends Filter {
-  def apply(nextFilter: (RequestHeader) => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
-    requestHeader.headers.get(USER_AGENT) match {
-      case Some(userAgent) if blacklist.contains(userAgent) => Future.successful(Results.Redirect(redirectTo, TEMPORARY_REDIRECT))
-      case _ => nextFilter(requestHeader)
-    }
-  }
+	def apply(nextFilter: (RequestHeader) => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
+		requestHeader.headers.get(USER_AGENT) match {
+			case Some(userAgent) if blacklist.contains(userAgent) => Future.successful(Results.Redirect(redirectTo, TEMPORARY_REDIRECT))
+			case _ => nextFilter(requestHeader)
+		}
+	}
 }
 
 class JorgeeBGoneModule extends Module {
-  def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
-    lazy val blacklist = new TypesafeBlacklistProvider(
-      "jorgeebgone.blacklist",
-      configuration.underlying
-    )
-    val redirectTo = configuration.getString("jorgeebgone.redirectTo").fold("https://github.com/")(identity)
-    Seq(
-      bind[BlacklistProvider].toInstance(blacklist),
-      bind[String].qualifiedWith("jorgeebgone.redirectTo").toInstance(redirectTo)
-    )
-  }
+	def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
+		lazy val blacklist = new TypesafeBlacklistProvider(
+			"jorgeebgone.blacklist",
+			configuration.underlying
+		)
+		val redirectTo = configuration.getString("jorgeebgone.redirectTo").fold("https://github.com/")(identity)
+		Seq(
+			bind[BlacklistProvider].toInstance(blacklist),
+			bind[String].qualifiedWith("jorgeebgone.redirectTo").toInstance(redirectTo)
+		)
+	}
 }
